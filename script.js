9754cbc4d54ef705e94c857b64576c6d96ec9746
AdrielSoares <adrielnunessoares2000@gmail.com>
2023-11-09 21:42:22 +0000

    fix: adicionar mais elementos e modificiaçao no JS

--- script.js
+++ script.js
@@ -26,3 +26,107 @@ function handleProfileFormSubmit(evt) {
   }
   
   form.addEventListener("submit", handleProfileFormSubmit);
+
+
+function addCard(cardData) {
+    const templateCard = document.querySelector("#template-card");
+    const cardElement = templateCard.content.cloneNode(true);
+  
+    const nameElement = cardElement.querySelector(".elements__description-text");
+    nameElement.textContent = cardData.name;
+    
+    const imageElement = cardElement.querySelector(".elements__card-image");
+    imageElement.src = cardData.image;
+    imageElement.alt = cardData.name;
+  
+    const popupImage = document.querySelector(".popup__image");
+  
+    imageElement.addEventListener("click", () => {
+      const popupZoom = document.querySelector(".popup__image-zoom");
+      popupZoom.src = cardData.image;
+      const popupDescription = document.querySelector(".popup__description-text");
+      popupDescription.textContent = cardData.name;
+      popupImage.classList.add("popup_opened");
+    });
+    
+    const closeZoom = document.querySelector(".popup_close-zoom");
+    closeZoom.addEventListener("click", () => {
+      popupImage.classList.remove("popup_opened");
+    })
+  
+    const cardList = document.querySelector(".elements__card");
+    cardList.prepend(cardElement);
+  
+    const trashButton = document.querySelector(".elements__button-trash");
+    trashButton.addEventListener("click", () => {
+      const removeElement = trashButton.closest(".elements__card-li");
+      removeElement.remove();
+    });
+  
+    const likeButton = document.querySelector(".elements__button-like");
+    likeButton.addEventListener("click", () => {
+      likeButton.classList.toggle("elements__button-like_active");
+    });
+  }
+
+  const initialCards = [
+    {
+      name: "Vale de Yosemite",
+      image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    },
+    {
+      name: "Lago Louise",
+      image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    },
+    {
+      name: "Montanhas Carecas",
+      image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+    },
+    {
+      name: "Parque Nacional da Vanoise ",
+      image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+    }
+];
+
+initialCards.forEach(addCard);
+
+const addPopupButton = document.querySelector(".profile__add-button");
+const inputTitle = document.querySelector(".popup__form-input_title");
+const inputImage = document.querySelector(".popup__form-input_link");
+const popupAdd = document.querySelector(".popup__add");
+const closePopupAddButton = document.querySelector(".popup_close");
+
+addPopupButton.addEventListener("click", () => {
+  popupAdd.classList.add("popup_opened");
+  inputTitle.value = "";
+  inputImage.value = "";
+});
+
+closePopupAddButton.addEventListener("click", function() {
+  popupAdd.classList.remove("popup_opened");
+});
+
+const formAdd = document.querySelector(".popup__form_add");
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  const inputTitle = document.querySelector(".popup__form-input_title");
+  const inputImage = document.querySelector(".popup__form-input_link");
+
+  const cardData = {
+    name: inputTitle.value,
+    image: inputImage.value,
+  };
+  
+  addCard(cardData);
+  popupAdd.classList.remove("popup_opened");
+} 
+formAdd.addEventListener("submit", handleCardFormSubmit);
